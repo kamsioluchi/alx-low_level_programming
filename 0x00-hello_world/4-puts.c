353426cd97055cbee6b192f07019ae3e87292121
kamsioluchi <oluchiagbo64@gmail.com>
2023-02-16 13:57:24 -0800

    correction on Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A Programming is like building a multilingual puzzle
- n
+ * main - A Programming is food
  * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
