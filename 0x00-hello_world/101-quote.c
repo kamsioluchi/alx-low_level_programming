492eebc938cc5a724c91624fde026533b971a4e4
kamsioluchi <oluchiagbo64@gmail.com>
2023-02-16 15:56:35 -0800

    correction on task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Your program should return 1
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
